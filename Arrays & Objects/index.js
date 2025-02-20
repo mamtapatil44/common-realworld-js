35c92026ea38f83c6a8daff0c68fab60c9230a52
Mamta Patil <mamtapatil44@gmail.com>
2025-02-20 15:02:14 -0500

    flter

--- Arrays & Objects/index.js
+++ Arrays & Objects/index.js
@@ -17,3 +17,30 @@ return filteredResult
 console.log("filteration: ",filterByCuisine("Italian"))
 console.log("filteration: ",filterByCuisine("Japanese"))
 console.log("filteration: ",filterByCuisine("American"))
+
+console.log("===================================================================")
+
+// Restaurant Search by Rating
+// Task: Write a function filterByRating(minRating) that filters out restaurants with 
+// ratings lower than the given minRating.
+
+function filterByRating(minRating){
+    return restaurants.filter((item)=>item.rating >= minRating)
+}
+console.log("rating: ",filterByRating(4.5))
+console.log("rating: ",filterByRating(4.7))
+
+console.log("===================================================================")
+
+// 2. Restaurant Search by Cuisine and Rating
+// Task: Write a function filterByCuisineAndRating(cuisine, minRating) 
+// that filters restaurants based on both cuisine and rating.
+
+function filterByCuisineAndRating(cuisine,minRating){
+    return restaurants.filter((restro)=>(restro.cuisine === cuisine && restro.rating >= minRating))
+
+}
+
+console.log("filterByCuisineAndRating: ",filterByCuisineAndRating("Italian",4.7))
+
+console.log("===================================================================")
