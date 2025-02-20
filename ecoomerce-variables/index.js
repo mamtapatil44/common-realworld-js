6f1df2e984c9c1ac11b204737e007f6d7e4aa482
Mamta Patil <mamtapatil44@gmail.com>
2025-02-20 12:58:33 -0500

    variable and datatype realworld questions

--- ecoomerce-variables/index.js
+++ ecoomerce-variables/index.js
@@ -18,3 +18,61 @@ function createProduct(name,price,isStock){
 
 const product = createProduct("mobile",12000,true)
 console.log("product ",product)
+
+
+
+// 2. Employee Record System
+// Scenario:
+// You are developing an employee management system where each employee has a 
+// name, age, designation, and salary. Ensure type safety while creating an employee record.
+
+
+function createEmployee(name,age,designation,salary){
+    if(typeof name !== "string" || typeof age !== "number" || typeof designation !== "string" || typeof salary !== "number"){
+     throw new Error("Invalid entries......")
+    }
+
+
+    return{
+        name,
+        age,
+        designation,
+        salary
+    }
+}
+
+const employee1 = createEmployee("mamta",30,"software engineer",8000000);
+console .log("emp1 ",employee1)
+// const employee = createEmployee("mamta","30","software engineer",8000000);
+// console .log("emp ",employee) // errors bacz of age type
+
+
+
+
+// 2. Movie Database
+// Scenario:
+// A movie streaming platform needs a system to store movie details, 
+// including the title, release year, genre, and IMDB rating
+
+
+function createMovie(title,reYear,genre,rating){
+
+    if(typeof title !== "string"|| typeof reYear !== "number" || typeof genre !== "string" || typeof rating !== "number"){
+      throw new Error("Wrong data.......");
+      
+    }
+
+
+    return {
+        title,
+        reYear,
+        genre,
+        rating
+
+    }
+
+}
+
+
+const movie = createMovie("ved",2022,"lovestory",5)
+console.log("movie=== ",movie)
